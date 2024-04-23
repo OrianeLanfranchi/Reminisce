213dbdc80ac3f68ba86e1364fe0903937a28d8ef
OrianeLanfranchi <131607571+OrianeLanfranchi@users.noreply.github.com>
2024-04-23 14:50:53 +0200

    Door (zooum out/in)
    TODO Ori : interaction

---
+++ Assets/Scripts/AutomaticAction.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AutomaticAction : MonoBehaviour
+{
+    public bool isInRange;
+    public UnityEvent interactAction;
+    public UnityEvent leavetAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isInRange)
+        {
+            interactAction.Invoke();
+        }
+
+        if (!isInRange)
+        {
+            leavetAction.Invoke();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            Debug.Log("Player is in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            Debug.Log("Player is not in range");
+        }
+    }
+}
---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,25 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    [SerializeField] public CinemachineVirtualCamera cam;
+
+    public virtual void zoomOutCamera()
+    {
+        Debug.Log("Zoom out");
+        //cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 20.0f, Time.deltaTime);
+        cam.m_Lens.OrthographicSize = Mathf.Lerp(cam.m_Lens.OrthographicSize, 25.0f, Time.deltaTime);
+    }
+
+    public virtual void zoomInCamera()
+    {
+        Debug.Log("Zoom in");
+        cam.m_Lens.OrthographicSize = Mathf.Lerp(cam.m_Lens.OrthographicSize, 12.22f, Time.deltaTime);
+    }
+
+
+}
