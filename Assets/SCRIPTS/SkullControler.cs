12d0fc41dde54eaa66c22ae0f2d4c36e13e971dc
OrianeLanfranchi <131607571+OrianeLanfranchi@users.noreply.github.com>
2024-05-08 15:58:41 +0200

    bug dialogue skull solved (again) - +dialogue Door
    flemme de faire un input user en fait)

--- Assets/SCRIPTS/SkullControler.cs
+++ Assets/SCRIPTS/SkullControler.cs
@@ -8,7 +8,7 @@ public class SkullControler : MonoBehaviour
     [SerializeField] Animator Skull_Animator;
     public virtual void onInteract()
     {
-        Debug.Log("Light up");
+        Debug.Log("Sk - Light up - chat cpt");
         Skull_Animator.SetBool("isActivated", true);
     }
 }
